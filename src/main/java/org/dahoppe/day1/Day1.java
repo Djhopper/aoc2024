cfa82d2dd3dc03d7296a81721c39e6a5fa3b9114
djhopper <djhopper@hotmail.co.uk>
2024-12-02 17:29:19 +0000

    Initial set-up and day 1

---
+++ src/main/java/org/dahoppe/day1/Day1.java
@@ -0,0 +1,56 @@
+package org.dahoppe.day1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+public class Day1 {
+
+    private static final Logger log = LoggerFactory.getLogger(Day1.class);
+
+    public record Input(List<Integer> left, List<Integer> right) {}
+
+    public static Input readInputFromResource(String filename) throws IOException {
+        String rawInput = new String(Day1.class.getResourceAsStream("/input/" + filename).readAllBytes());
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        String[] lines = rawInput.split("\\r\\n");
+        log.info("Reading {} lines of input", lines.length);
+
+        for (String line: lines) {
+            String[] numbers = line.split("   ");
+            assert numbers.length == 2;
+            left.add(Integer.parseInt(numbers[0]));
+            right.add(Integer.parseInt(numbers[1]));
+        }
+
+        return new Input(left, right);
+    }
+
+    public static int solveA(Input input) {
+        List<Integer> sortedLeft = input.left().stream().sorted().toList();
+        List<Integer> sortedRight = input.right().stream().sorted().toList();
+        int total = 0;
+        for (int i=0; i<sortedLeft.size(); i++) {
+            total += Math.abs(sortedLeft.get(i) - sortedRight.get(i));
+        }
+        return total;
+    }
+
+    public static long solveB(Input input) {
+        Map<Integer, Long> numberOfOccurencesInRightList = input.right().stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return input.left().stream()
+                .mapToLong(number -> number * numberOfOccurencesInRightList.getOrDefault(number, 0L))
+                .sum();
+    }
+
+}
---
+++ src/test/java/org/dahoppe/day1/Day1Test.java
@@ -0,0 +1,45 @@
+package org.dahoppe.day1;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Day1Test {
+
+    private static final Day1.Input EXAMPLE_INPUT = new Day1.Input(
+            List.of(3, 4, 2, 1, 3, 3),
+            List.of(4, 3, 5, 3, 9, 3)
+    );
+
+    @Test
+    void testReadInput() throws IOException {
+        assertThat(new Day1().readInputFromResource("1a.txt")).satisfies(input -> {
+            assertThat(input.left()).hasSize(1000);
+            assertThat(input.right()).hasSize(1000);
+        });
+    }
+
+    @Test
+    void testSolveAExample() {
+        assertThat(Day1.solveA(EXAMPLE_INPUT)).isEqualTo(11);
+    }
+
+    @Test
+    void testSolveAPuzzle() throws IOException {
+        assertThat(Day1.solveA(Day1.readInputFromResource("1a.txt"))).isEqualTo(2086478);
+    }
+
+    @Test
+    void testSolveBExample() {
+        assertThat(Day1.solveB(EXAMPLE_INPUT)).isEqualTo(31);
+    }
+
+    @Test
+    void testSolveBPuzzle() throws IOException {
+        assertThat(Day1.solveB(Day1.readInputFromResource("1a.txt"))).isEqualTo(24941624);
+    }
+
+}
